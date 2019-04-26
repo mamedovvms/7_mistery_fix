4368033cdc7a5a9f14f1b9ba78ff2f76f079a775
1237 <mamedov_vs@oksw.ru>
2019-04-26 09:31:11 +0700

    Заменена конструкция try..exept на if .. else

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,14 +1,24 @@
 from math import sqrt
 
 
+def main():
+    root1, root2 = get_roots(1, 1, -6)
+    print(root1, root2)
+
+    root1, root2 = get_roots(1, 2, 3)
+    print(root1, root2)
+
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    try:
+    root1, root2 = None, None
+
+    if discriminant > 0:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
         root2 = (-b + sqrt(discriminant)) / (2 * a)
-    except ValueError:
-        return None, None
-    if discriminant == 0:
-        return root1, None
-    else:
-        return root1, root2
+    elif discriminant == 0:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+    return root1, root2
+
+
+if __name__ == '__main__':
+    main()
