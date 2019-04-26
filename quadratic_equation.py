40fb67e17fe7abd75424e3111199a7b3466f2fc9
1237 <mamedov_vs@oksw.ru>
2019-04-26 11:00:06 +0700

    Удалена функция main()

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,13 +1,6 @@
 from math import sqrt
 
 
-def main():
-    root1, root2 = get_roots(1, 1, -6)
-    print(root1, root2)
-
-    root1, root2 = get_roots(1, 2, 3)
-    print(root1, root2)
-
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     root1, root2 = None, None
@@ -18,7 +11,3 @@ def get_roots(a, b, c):
     elif discriminant == 0:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
     return root1, root2
-
-
-if __name__ == '__main__':
-    main()
